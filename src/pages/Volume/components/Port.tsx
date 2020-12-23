2b503f156fa3f541523a506c8e8b605403dbc3be
Iverson.Tian <iverson.tian@outlook.com>
2020-12-23 18:27:11 +0800

    Update Port.tsx

--- src/pages/Volume/components/Port.tsx
+++ src/pages/Volume/components/Port.tsx
@@ -115,6 +115,14 @@ const Port: React.FC<{}> = () => {
                     }
                 }
             },
+            toolbox: {
+                feature: {
+                    dataView: { show: true, readOnly: false },
+                    magicType: { show: true, type: ['line', 'bar'] },
+                    restore: { show: true },
+                    saveAsImage: { show: true }
+                }
+            },
             grid: {
                 left: '3%',
                 right: '4%',
