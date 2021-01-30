d3941cc3576a9ed5fe4ebb266f5434095332a7cd
Iverson.Tian <iverson.tian@outlook.com>
2021-01-30 21:25:05 +0800

    前10港口提交

--- src/pages/Volume/components/Port.tsx
+++ src/pages/Volume/components/Port.tsx
@@ -13,7 +13,8 @@ import 'echarts/lib/component/tooltip';
 import { getPortChartData, } from '@/services/volume';
 //调用公式方法
 import { sortObjectArr, transIntOfArraay, calculateOfArraay, FilterZeroOfArraay, } from '@/utils/utils';
-import { getselectBranchID, getselectYear, } from '@/utils/auths';
+import { OceanTransportTypeList,} from '@/utils/baseData';
+import { getselectBranchID, getselectYear, getselectBusinessesLine, getselectBizType1List_Radio, getselectOceanTransportType, } from '@/utils/auths';
 //引入自定义组件
 import SearchButton from '@/components/Search/SearchButton';
 //重点代码<React hooks之useContext父子组件传值>
@@ -21,84 +22,57 @@ import ContextProps from '@/createContext';
 
 const VolumePort: React.FC<{}> = () => {
     const { initialState, } = useModel('@@initialState');
+    const [OceanTransportType,] = useState(() => {
+        // 惰性赋值 any 类型,要不默认值不起作用
+        let selectOceanTransportType: any = getselectOceanTransportType();
+        return selectOceanTransportType;
+    });
     const [loading, setloading] = useState(false);
-    const [type, setType] = useState('收入');
-    const [result, setResult] = useState([]);
 
     //获取数据
-    let fetchData = async (ParamsInfo: any, T: string) => {
+    let fetchData = async (ParamsInfo: any) => {
         setloading(true);
         const result = await getPortChartData(ParamsInfo);
         if (!result || getselectBranchID() == '') {
             return;
         }
         if (result) {
-            if (result.length > 0) {
-                setResult(result);
-            }
             //将值传给初始化图表的函数
-            initChart(result, T);
+            initChart(result);
             setloading(false);
         }
     }
 
     //初始化图表
     let myChart: any;
-    let initChart = (result: any, type: string) => {
+    let initChart = (result: any) => {
         let element = document.getElementById('VolumePort');
         if (myChart != null && myChart != "" && myChart != undefined) {
             myChart.dispose();
         }
         let option: any;
 
-        // 根据 type 排序
+        //处理数据
         let PortTopList: any = [];
-        let PortTopTotalARList: any = [];
-        let PortTopTotalFCLList: any = [];
-        let PortTopTotalLCLList: any = [];
-        let PortTopTotalBulkList: any = [];
+        let PortTopTotalVolumeList: any = [];
         let PortTopPortNameList: any = [];
-        if (type == '收入') {
-            PortTopList = FilterZeroOfArraay((result.sort(sortObjectArr('TotalAR', 2)).slice(0, 10)).sort(sortObjectArr('TotalAR', 1)), 10000, 'TotalAR');
-        } else if (type == '整箱') {
-            PortTopList = FilterZeroOfArraay((result.sort(sortObjectArr('FLCVolume', 2)).slice(0, 10)).sort(sortObjectArr('FLCVolume', 1)), 0, 'FLCVolume');
-        } else if (type == '拼箱') {
-            PortTopList = FilterZeroOfArraay((result.sort(sortObjectArr('LCLVolume', 2)).slice(0, 10)).sort(sortObjectArr('LCLVolume', 1)), 0, 'LCLVolume');
-        } else {
-            PortTopList = FilterZeroOfArraay((result.sort(sortObjectArr('BulkVolume', 2)).slice(0, 10)).sort(sortObjectArr('BulkVolume', 1)), 0, 'BulkVolume');
-        }
+        PortTopList = FilterZeroOfArraay((result.sort(sortObjectArr('Volume', 2)).slice(0, 10)).sort(sortObjectArr('Volume', 1)), 0, 'Volume');
         if (PortTopList.length > 0) {
-            PortTopList.map((x: { TotalAR: any; FLCVolume: any; LCLVolume: any; BulkVolume: any; PortName: string; }) => {
-                PortTopTotalARList.push((x.TotalAR / 10000).toFixed(2));
-                PortTopTotalFCLList.push(x.FLCVolume);
-                PortTopTotalLCLList.push(x.LCLVolume);
-                PortTopTotalBulkList.push(x.BulkVolume);
+            PortTopList.map((x: { Volume: any; PortName: string; }) => {
+                PortTopTotalVolumeList.push(x.Volume);
                 PortTopPortNameList.push(x.PortName);
             });
         }
-        // 赋值图表 series 和 yAxis 中的 name
-        let seriesData = [];
         let yAxisName = '';
-        if (type == '收入') {
-            seriesData = [...PortTopTotalARList];
-            yAxisName = '单位: CNY(万)';
-        } else if (type == '整箱') {
-            seriesData = [...PortTopTotalFCLList];
-            yAxisName = '单位: TEU';
-        } else if (type == '拼箱') {
-            seriesData = [...PortTopTotalLCLList];
-            yAxisName = '单位: CBM';
-        } else {
-            //后台散货存的是 KGS
-            seriesData = calculateOfArraay(PortTopTotalBulkList, '/', 1000);
-            yAxisName = '单位: TON';
+        if(PortTopList.length > 0){
+            yAxisName = PortTopList[0].VolUnit;
         }
 
         if (element) {
             myChart = echarts.init(element as HTMLDivElement);
             option = {
                 title: {
-                    text: `前10港口${type}排名`,
+                    text: `前10港口${OceanTransportTypeList.find(x=>x.Key == OceanTransportType)?.Value}排名`,
                 },
                 tooltip: {
                     trigger: 'axis',
@@ -131,7 +105,7 @@ const VolumePort: React.FC<{}> = () => {
                 },
                 yAxis: {
                     type: 'category',
-                    name: yAxisName,
+                    name: `单位: ${yAxisName}`,
                     scale: true,
                     axisLabel: {
                         show: true,
@@ -147,7 +121,7 @@ const VolumePort: React.FC<{}> = () => {
                 series: [
                     {
                         type: 'bar',
-                        name: type,
+                        name: yAxisName,
                         color: '#C23531',
                         label: {
                             show: true,
@@ -162,7 +136,7 @@ const VolumePort: React.FC<{}> = () => {
                                 }
                             },
                         },
-                        data: transIntOfArraay(seriesData),
+                        data: transIntOfArraay(PortTopTotalVolumeList),
                     }
                 ],
             };
@@ -180,36 +154,19 @@ const VolumePort: React.FC<{}> = () => {
             BranchID: getselectBranchID(),
             Year: getselectYear(),
             Months: initialState?.searchInfo?.MonthList || [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-            TransTypes: initialState?.searchInfo?.BizType1List_MultiSelect || [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14],
+            TransTypes: [getselectBizType1List_Radio()],
             TradeTypes: initialState?.searchInfo?.BizType2List || [1, 2, 3, 4, 5, 6],
+            CargoTypes: [getselectOceanTransportType()],
+            BizLines: [getselectBusinessesLine()],
         };
         if (getselectBranchID() !== '') {
-            fetchData(ParamsInfo, type);
+            fetchData(ParamsInfo);
         }
     }, [initialState]);
 
-    /**
-     * 点击切换统计图表类型
-     */
-    const onChangeType = (e: any) => {
-        setType(e.target.value);
-        initChart(result, e.target.value);
-    }
-
     return <>
         <Spin tip="数据正在加载中,请稍等..." spinning={loading}>
-            <Card
-                extra={
-                    <>
-                        <Radio.Group defaultValue={type} buttonStyle="solid" onChange={onChangeType}>
-                            <Radio.Button value="收入">收入</Radio.Button>
-                            <Radio.Button value="整箱">整箱</Radio.Button>
-                            <Radio.Button value="拼箱">拼箱</Radio.Button>
-                            <Radio.Button value="散货">散货</Radio.Button>
-                        </Radio.Group>
-                    </>
-                }
-            >
+            <Card>
                 <div id="VolumePort" style={{ width: '100%', height: 800 }}></div>
             </Card>
         </Spin>
