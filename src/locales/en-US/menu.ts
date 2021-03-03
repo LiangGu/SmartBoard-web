e89f8dc19003332fd89163666086d100e4601884
Iverson.Tian <iverson.tian@outlook.com>
2021-03-03 16:54:09 +0800

    修改menu菜单翻译

--- src/locales/en-US/menu.ts
+++ src/locales/en-US/menu.ts
@@ -5,7 +5,7 @@ export default {
   'menu.volume.month': '月份',
   'menu.volume.port': '港口',
   'menu.icprofit': '收支毛利',
-  'menu.cashflow': '现金流',
+  'menu.cashflow': '业务收支现金流',
   'menu.debt': '应收账款',
   'menu.rank': '客户排名',
 };
