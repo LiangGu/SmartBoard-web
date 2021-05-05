e4c680da9b76ac1e14d00fbef814bb214dca4d60
Iverson.Tian <iverson.tian@outlook.com>
2021-05-05 15:52:02 +0800

    Update baseData.ts

--- src/utils/baseData.ts
+++ src/utils/baseData.ts
@@ -2,77 +2,77 @@
  * MonthList
  */
 export const MonthList = [
-    { Key: 1, Value: "1月" },
-    { Key: 2, Value: "2月" },
-    { Key: 3, Value: "3月" },
-    { Key: 4, Value: "4月" },
-    { Key: 5, Value: "5月" },
-    { Key: 6, Value: "6月" },
-    { Key: 7, Value: "7月" },
-    { Key: 8, Value: "8月" },
-    { Key: 9, Value: "9月" },
-    { Key: 10, Value: "10月" },
-    { Key: 11, Value: "11月" },
-    { Key: 12, Value: "12月" },
-]
+  { Key: 1, Value: '1月' },
+  { Key: 2, Value: '2月' },
+  { Key: 3, Value: '3月' },
+  { Key: 4, Value: '4月' },
+  { Key: 5, Value: '5月' },
+  { Key: 6, Value: '6月' },
+  { Key: 7, Value: '7月' },
+  { Key: 8, Value: '8月' },
+  { Key: 9, Value: '9月' },
+  { Key: 10, Value: '10月' },
+  { Key: 11, Value: '11月' },
+  { Key: 12, Value: '12月' },
+];
 
 /**
  * BusinessesLineList
  */
 export const BusinessesLineList = [
-    { Key: 1, Value: "工程" },
-    { Key: 2, Value: "合同" },
-    { Key: 3, Value: "电商" },
-    { Key: 4, Value: "船代" },
-    { Key: 5, Value: "货代" },
-]
+  { Key: 1, Value: '工程' },
+  { Key: 2, Value: '合同' },
+  { Key: 3, Value: '电商' },
+  { Key: 4, Value: '船代' },
+  { Key: 5, Value: '货代' },
+];
 
 /**
  * BizType1List_MultiSelect
  */
 export const BizType1List_MultiSelect = [
-    { Key: 1, Value: "水运" },
-    { Key: 2, Value: "空运" },
-    { Key: 3, Value: "陆运" },
-    { Key: 4, Value: "铁路" },
-    { Key: 5, Value: "多式联运" },
-    { Key: 6, Value: "仓储" },
-    { Key: 10, Value: "散货船" },
-    { Key: 11, Value: "总代" },
-    { Key: 12, Value: "空海跨境" },
-    { Key: 13, Value: "增值服务" },
-    { Key: 14, Value: "货运站" },
-]
+  { Key: 1, Value: '水运' },
+  { Key: 2, Value: '空运' },
+  { Key: 3, Value: '陆运' },
+  { Key: 4, Value: '铁路' },
+  { Key: 5, Value: '多式联运' },
+  { Key: 6, Value: '仓储' },
+  { Key: 10, Value: '散货船' },
+  { Key: 11, Value: '总代' },
+  { Key: 12, Value: '空海跨境' },
+  { Key: 13, Value: '增值服务' },
+  { Key: 14, Value: '货运站' },
+];
 
 /**
  * BizType2List
  */
 export const BizType2List = [
-    { Key: 1, Value: "出口" },
-    { Key: 2, Value: "进口" },
-    { Key: 3, Value: "内贸" },
-    { Key: 4, Value: "第三国" },
-    { Key: 5, Value: "转口" },
-    { Key: 6, Value: "过境" },
-]
+  { Key: 1, Value: '出口' },
+  { Key: 2, Value: '进口' },
+  { Key: 3, Value: '内贸' },
+  { Key: 4, Value: '第三国' },
+  { Key: 5, Value: '转口' },
+  { Key: 6, Value: '过境' },
+];
 
 /**
  * OceanTransportTypeList_MultiSelect
  */
 export const OceanTransportTypeList_MultiSelect = [
-    // { Key: 0, Value: "不限" },                      //传 0 取全部
-    { Key: 1, Value: "整箱" },
-    { Key: 2, Value: "拼箱" },
-    { Key: 3, Value: "散货" },
-    { Key: 6, Value: "整车" },
-    { Key: 7, Value: "零担" },
-]
+  // { Key: 0, Value: "不限" },                      //传 0 取全部
+  { Key: 1, Value: '整箱' },
+  { Key: 2, Value: '拼箱' },
+  { Key: 3, Value: '散货' },
+  { Key: 6, Value: '整车' },
+  { Key: 7, Value: '零担' },
+];
 
 /**
  * BranchList
  */
 // export const BranchList = [
-//     { Key: 0, Value: "香港外运(总部)" },             //香港总部传 0 
+//     { Key: 0, Value: "香港外运(总部)" },             //香港总部传 0
 //     { Key: 2, Value: "香港船务" },
 //     { Key: 5, Value: "上海伟运货代" },
 //     { Key: 6, Value: "泰国外运" },
@@ -95,21 +95,22 @@ export const OceanTransportTypeList_MultiSelect = [
  * 人力资源公司数据
  */
 export const HRBranchList = [
-    { branchName: "香港物流" },
-    { branchName: "(深圳)供应链" },
-    { branchName: "工程事业部" },
-    { branchName: "香港船务" },
-    { branchName: "上海伟运" },
-    { branchName: "泰国公司" },
-    { branchName: "马来西亚公司" },
-    { branchName: "香港仓储" },
-    { branchName: "印尼公司" },
-    { branchName: "越南公司" },
-    { branchName: "缅甸公司" },
-    { branchName: "菲律宾公司" },
-    { branchName: "柬埔寨公司" },
-    { branchName: "大宗商品" },
-    { branchName: "电子商务部" },
-    { branchName: "空运事业部" },
-    { branchName: "供应链金融事业部" },
-]
+  { branchName: '香港物流' },
+  { branchName: '(深圳)供应链' },
+  { branchName: '工程事业部' },
+  { branchName: '香港船务' },
+  { branchName: '上海伟运' },
+  { branchName: '泰国公司' },
+  { branchName: '马来西亚公司' },
+  { branchName: '香港仓储' },
+  { branchName: '印尼公司' },
+  { branchName: '越南公司' },
+  { branchName: '缅甸公司' },
+  { branchName: '菲律宾公司' },
+  { branchName: '柬埔寨公司' },
+  { branchName: '大宗商品' },
+  { branchName: '电子商务部' },
+  { branchName: '空运事业部' },
+  { branchName: '供应链金融事业部' },
+  { branchName: '上海共享中心' },
+];
